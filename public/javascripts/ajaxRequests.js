246d7b32ec3fa91e8d504a01a13a851cb8910207
Andrea <Andrea.Tranchina82@gmail.com>
2023-07-13 15:52:10 -0400

    improved get and post features to machines table

--- public/javascripts/ajaxRequests.js
+++ public/javascripts/ajaxRequests.js
@@ -2,24 +2,32 @@
 
 // const axios = require('axios');
 
+const machinesTable = [];
+
 const fetchDataButton = document.querySelector('.fetch-data-button');
+
 const machinesDataContainer = document.querySelector('.machines-data-container');
-const postButton = document.querySelector('.post-button');
-const userInput = document.querySelector('.user-input');
-const snInput = document.querySelector('.sn-nput');
-const tankLitersInput = document.querySelector('.tank-liters-input');
+const postDataButton = document.querySelector('.post-data-button');
+
+let userInput = document.querySelector('#user-input');
+let snInput = document.querySelector('#sn-input');
+let tankLitersInput = document.querySelector('#tank-liters-input');
+let isFetched = false;
+let isEditing = false;
+
+const table = document.createElement("table");
+table.style.border = "1px solid black";
+machinesDataContainer.appendChild(table);
 
 const handleClickGet = async () => {
+
     console.log("i was clicked");
-    try{
+    if (isFetched === false) {
+      try{
         const res = await fetch("http://localhost:3001/machines")
         const resJson = await res.json();
         // const resJson2 = await JSON.parse(res)
         console.log(resJson);
-        console.log(resJson[0].sn)
-
-        const table = document.createElement("table");
-        table.style.border = "1px solid black";
 
         const tableHead = document.createElement("thead");
         const tableHeadRow = document.createElement("tr");
@@ -33,9 +41,17 @@ const handleClickGet = async () => {
         const tableHeadUser = document.createElement("td");
         tableHeadUser.innerText = "User";
 
+        const tableHeadEdit = document.createElement("td");
+        tableHeadEdit.innerText = "Edit";
+
+        const tableHeadDelete = document.createElement("td");
+        tableHeadDelete.innerText= "Delete";
+
         tableHeadRow.appendChild(tableHeadSn);
         tableHeadRow.appendChild(tableHeadTankLiters);
         tableHeadRow.appendChild(tableHeadUser);
+        tableHeadRow.appendChild(tableHeadEdit);
+        tableHeadRow.appendChild(tableHeadDelete);
 
         tableHead.appendChild(tableHeadRow);
         table.appendChild(tableHead);
@@ -51,64 +67,286 @@ const handleClickGet = async () => {
     
             const tableCellUser = document.createElement("td");
             tableCellUser.innerHTML = resJson[i].user;
-    
+
+            const tableCellEdit = document.createElement("td");
+            tableCellEdit.innerHTML = "✏️";
+            tableCellEdit.addEventListener("click", () => {
+              isEditing = true;
+              showEditTable();
+            })
+
+            const tableCellDelete = document.createElement("td");
+            tableCellDelete.innerHTML = "🗑️";
+
             tableRow.appendChild(tableCellSn);
             tableRow.appendChild(tableCellTankLiters);
             tableRow.appendChild(tableCellUser);
+            tableRow.appendChild(tableCellEdit);
+            tableRow.appendChild(tableCellDelete);
     
             table.appendChild(tableRow);
 
+            machinesTable.push({
+              id: resJson[i].id,
+              snCell: tableCellSn,
+              tankLitersCell: tableCellTankLiters,
+              user: tableCellUser,
+              editButton: tableCellEdit,
+              deleteButton: tableCellDelete
+            });
+            console.log(machinesTable);
+
+            tableCellDelete.addEventListener("click", (event) => handleClickDelete(event, resJson[i].id));
         }
-
-        machinesDataContainer.appendChild(table);
-
-        // const dataItem = document.createElement("h1");
-        // dataItem.innerText = resJson[0].sn;
-        // dataItem.style.fontSize = "3rem";
-        // machinesDataContainer.appendChild(dataItem);
-    }
-    catch(error){
+        isFetched = true;
+      }
+      catch(error){
         console.log(error);
+      }
     }
-
 }
 
 fetchDataButton.addEventListener("click", handleClickGet);
 
 
-const handleClickPost = async (event) => {
-    event.preventDefault();
-    console.log("post button was clicked");
-    
-    try {
+
+/*********TRACK CHANGES IN INPUTS FOR POST/ADD****************/
+
+let sn = "";
+let user = "";
+let tankLiters = "";
+
+const handleInputSn = (event) => {
+  // sn = snInput.value;
+  sn = event.target.value;
+  console.log("i was changed");
+  console.log(sn);
+}
+
+const handleInputUser = (event) => {
+  user = event.target.value;
+  console.log(user);
+}
+
+const handleInputTankLiters = (event) => {
+  tankLiters = event.target.value;
+  console.log(tankLiters);
+}
+
+userInput.addEventListener("input", handleInputUser);
+snInput.addEventListener("input", handleInputSn);
+tankLitersInput.addEventListener("input", handleInputTankLiters);
+
+//********* POST / ADD ***************/
+
+const handleClickPost = async () => {
+  console.log("post was clicked");
+  console.log(sn);
+
+      try {
+        const newMachine = {
+          sn: sn,
+          tankLiters: tankLiters,
+          user: user,
+        }
+
         const response = await fetch("http://localhost:3001/machines", {
           method: "POST", // or 'PUT'
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(data),
+          body: JSON.stringify(newMachine),
         });
     
         const responseJson = await response.json();
         console.log("Success:", responseJson);
+
+        if(isFetched){
+          const tableRow = document.createElement("tr");
+          tableRow.style.border = "1px solid black";
+          const tableCellSn = document.createElement("td");
+          tableCellSn.innerText = responseJson.sn;
+  
+          const tableCellTankLiters = document.createElement("td");
+          tableCellTankLiters.innerHTML = responseJson.tankLiters;
+  
+          const tableCellUser = document.createElement("td");
+          tableCellUser.innerHTML = responseJson.user;
+
+          const tableCellEdit = document.createElement("td");
+          tableCellEdit.innerHTML = "✏️";
+          tableCellEdit.addEventListener("click", () => {
+            isEditing = true;
+            showEditTable();
+          })
+
+          const tableCellDelete = document.createElement("td");
+          tableCellDelete.innerHTML = "🗑️";
+  
+          tableRow.appendChild(tableCellSn);
+          tableRow.appendChild(tableCellTankLiters);
+          tableRow.appendChild(tableCellUser);
+          tableRow.appendChild(tableCellEdit);
+          tableRow.appendChild(tableCellDelete);
+  
+          table.appendChild(tableRow);
+
+          machinesTable.push({
+            id: responseJson.id,
+            sn: tableCellSn,
+            tankLiters: tableCellTankLiters,
+            user: tableCellUser,
+            editButton: tableCellEdit,
+            deleteButton: tableCellDelete,
+          });
+
+          tableCellDelete.addEventListener("click", (event) => handleClickDelete(event, responseJson.id));
+        }
+
+        snInput.value = "";
+        userInput.value = "";
+        tankLitersInput.value = "";
+
       } catch (error) {
         console.error("Error:", error);
       }
 
-
-
 }
 
-postButton.addEventListener("click", handleClickPost)
+postDataButton.addEventListener("click", handleClickPost);
+
+
+//Show post table
+// const ShowPostTable = () => {
+//   if(isFetched){
+//     const postHeader = document.createElement("h2");
+//     postHeader.innerHTML = "Post Form"
+
+//     const postForm = document.createElement("form")
+//     const postSn = document.createElement("input")
+//     const postUser = document.createElement("input")
+//     const postTankLiters = document.createElement("input");
+
+//     const submitPostButton = document.createElement("button");
+//     submitPostButton.innerHTML = "Submit New!";
+
+//     postTankLiters.setAttribute("id", "tank-liters-input");
+//     postSn.setAttribute("id", "sn-input");
+//     postSn.setAttribute("id", "user-input");
+
+//     postTankLiters.classList.add("input-post");
+//     postSn.classList.add("input-post");
+//     postUser.classList.add("input-post");
+//     submitPostButton.classList.add("post-data-button");
+
+//     postTankLiters.placeholder = "tankLiters";
+//     postUser.placeholder = "user";
+//     postSn.placeholder = "sn";
+  
+    
+//     postForm.append(postHeader, postSn, postTankLiters, postUser, submitPostButton);
+//     machinesDataContainer.appendChild(postForm);
+//   }
+// }
+
+
+//***********EDITING */
+
+const showEditTable = () =>{
+  if(isEditing){
+    const editHeader = document.createElement("h2");
+    editHeader.innerHTML = "Edit Form"
+
+    const editForm = document.createElement("form")
+    const editSnInput = document.createElement("input")
+    const editUserInput = document.createElement("input")
+    const editTankLitersInput = document.createElement("input");
+
+    const submitEditButton = document.createElement("button");
+    submitEditButton.innerHTML = "Submit Edits!";
+
+    editTankLitersInput.classList.add("input-post");
+    editSnInput.classList.add("input-post");
+    editUserInput.classList.add("input-post");
+    submitEditButton.classList.add("edit-data-button");
+
+    editTankLitersInput.placeholder = "tankLiters";
+    editUserInput.placeholder = "user";
+    editSnInput.placeholder = "sn";
+  
+    
+    editForm.append(editHeader, editSnInput, editTankLitersInput, editUserInput, submitEditButton);
+    machinesDataContainer.appendChild(editForm);
 
 
 
+    let currentEditSn = "";
+    let currentEditTankLiters = "";
+    let currentEditUser = "";
+
+    const handleInputEditSn = (event) => {
+      // sn = snInput.value;
+      currentEditSn = event.target.value;
+      console.log("i was changed");
+    }
+    
+    const handleInputEditUser = (event) => {
+      currentEditUser = event.target.value;
+    }
+    
+    const handleInputEditTankLiters = (event) => {
+      currentEditTankLiters = event.target.value;
+    }
 
 
+    const handleClickEdit = async (event) => {
+      try {
+        const editedMachine = {
+          sn: currentEditSn,
+          tankLiters: currentEditTankLiters,
+          user: currentEditUser,
+        }  
+
+        const response = await fetch("http://localhost:3001/machines", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(editedMachine),
+        });
+    
+        const responseJson = await response.json();
+        console.log("Success:", responseJson);
+
+        snInput.value = "";
+        userInput.value = "";
+        tankLitersInput.value = "";
+
+      } catch (error) {
+        console.error("Error:", error);
+      }
+
+    }  
+
+    userInput.addEventListener("input", handleInputEditUser);
+    snInput.addEventListener("input", handleInputEditSn);
+    tankLitersInput.addEventListener("input", handleInputEditTankLiters);
+    submitEditButton.addEventListener("click", handleClickEdit);
+  }
+}  
 
 
+const handleClickDelete = async (event, id) => {
+  console.log("will delete " + id);
 
-// fetchDataButton.addEventListener("click", () => {
-//     console.log("I was clicked");
-// });
-
+  try{
+    const response = await fetch(`http://localhost:3001/machines/${id}`, {
+      method: "DELETE",
+    });
+    const responseJson = await response.json();
+    console.log("Success:", responseJson);
+  }
+  catch(error){
+    console.log(error);
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -2,31 +2,39 @@ var express = require('express');
 const bodyParser = require('body-parser');
 var router = express.Router();
 
-router.use(bodyParser.urlencoded({extended:true}));
+// router.use(bodyParser.urlencoded({extended:true}));
 
-// router.use(express.json());
+router.use(express.json());
+
+let id = 1;
+
 const machines = [
-    {
+    {   
+        id: id++,
         sn: "thidfn234",
         tankLiters: 300,
         user: "Andrea"
     },
-    {
+    {   
+        id: id++,
         sn: "iverinre54",
         tankLiters: 500,
         user: "John"
     },
-    {
+    {   
+        id: id++,
         sn: "oiregero439",
         tankLiters: 750,
         user: "Linda",
     },
     {
+        id: id++,
         sn: "fner3490",
         tankLiters: 340,
         user: "Jacob",
     },
-    {
+    {   
+        id: id++,
         sn: "iofinw43003",
         tankLiters: 393,
         user: "Marcus", 
@@ -41,66 +49,66 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get("/machinesForm", function(req, res){
-    res.sendFile(__dirname + "/machines.html");
-    res.render('machines');
+// router.get("/machinesForm", function(req, res){
+//     res.sendFile(__dirname + "/machines.html");
+//     res.render('machines');
 
-});
+// });
 
-router.post("/machinesForm", function(req, res){
-    const sn = req.body.sn;
-    const user = req.body.user;
-    const tankLiters = Number(req.body.tankSize);
+// router.post("/machinesForm", function(req, res){
+//     const sn = req.body.sn;
+//     const user = req.body.user;
+//     const tankLiters = Number(req.body.tankSize);
 
-    const newMachine = {
-        sn: sn,
-        tankLiters: tankLiters,
-        user: user
-    }
-    console.log(newMachine);
-    machines.push(newMachine);
-    res.send(newMachine);
+//     const newMachine = {
+//         sn: sn,
+//         tankLiters: tankLiters,
+//         user: user
+//     }
+//     console.log(newMachine);
+//     machines.push(newMachine);
+//     res.send(newMachine);
 
-    res.set('Content-Type', 'text/html');
-    res.render('machines', { machines: machines });
-    res.send(machines.map((machine) => {
-        return(`
-        <table>
-        <thead>
-            <tr>
-                <td>sn</td>
-                <td>tank size (liters)</td>
-                <td>user</td>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>${machine.sn}</td>
-                <td>${machine.tankLiters}</td>
-                <td>${machine.user}</td>
-            </tr>
-        </tbody>
-    </table>`)       
-    })
+//     res.set('Content-Type', 'text/html');
+//     res.render('machines', { machines: machines });
+//     res.send(machines.map((machine) => {
+//         return(`
+//         <table>
+//         <thead>
+//             <tr>
+//                 <td>sn</td>
+//                 <td>tank size (liters)</td>
+//                 <td>user</td>
+//             </tr>
+//         </thead>
+//         <tbody>
+//             <tr>
+//                 <td>${machine.sn}</td>
+//                 <td>${machine.tankLiters}</td>
+//                 <td>${machine.user}</td>
+//             </tr>
+//         </tbody>
+//     </table>`)       
+//     })
 
-    );
+//     );
 
     
-});
+// });
 
 router.get('/machines', function(req, res, next) {
     res.json(machines);
 })
 
-router.get('/machines/1', function(req, res, next) {
-    res.send(machines[0].sn + machines[0].tankLiters);
-    console.log(machines[0].sn + machines[0].tankLiters);
-})
+// router.get('/machines/1', function(req, res, next) {
+//     res.send(machines[0].sn + machines[0].tankLiters);
+//     console.log(machines[0].sn + machines[0].tankLiters);
+// })
 
-router.get('/machines/2', function(req, res, next) {
-    res.send(machines[1].sn + machines[1].tankLiters);
-    console.log(machines[1].sn + machines[1].tankLiters)
-})
+// router.get('/machines/2', function(req, res, next) {
+//     res.send(machines[1].sn + machines[1].tankLiters);
+//     console.log(machines[1].sn + machines[1].tankLiters)
+// })
 
 router.get('/user/:username', function(req, res, next) {
     res.send("welcome: " + req.params.username);
@@ -112,16 +120,41 @@ router.get('/userquery', function(req, res, next) {
 
 router.post('/machines', function(req, res, next) {
     const newMachine = {
+        id: id++,
         sn: req.body.sn,
         tankLiters: req.body.tankLiters,
         user: req.body.user
     }
     machines.push(newMachine);
-    res.send(machines)
+    res.send(newMachine)
 })
 
+router.put('/machines/:id', function(req, res, next) {
+    const id = req.params.id;
 
+    if (!req.body.sn){
+        machines[id].sn = req.body.sn;
+    }
+    if (!req.body.tankLiters){
+        machines[id].tankLiters = req.body.tankLiters;
+    }
+    if (!req.body.user){
+        machines[id].user = req.body.user;
+    }
 
+    res.json(machines[id]);
+});
+
+router.delete('/machines/:id', function(req, res, next) {
+    console.log("running delete method route");
+    const id = req.params.id;
+    console.log("id parameter: " + id);
+
+    const index = machines.findIndex((machine) => machine.id === id);
+    machines.splice(index, 1);
+    
+    res.json(machines);
+})
 
 
 module.exports = router;
